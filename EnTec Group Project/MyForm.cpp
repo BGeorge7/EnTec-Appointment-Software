ec3f74a07199e35bbae26d0b79ced3b42a96b3ed
george <george@MR0REO>
2016-11-16 10:55:10 -0500

    Add project files.

---
+++ EnTec Group Project/MyForm.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	EnTec_Group_Project::MyForm form;
+	Application::Run(%form);
+}
